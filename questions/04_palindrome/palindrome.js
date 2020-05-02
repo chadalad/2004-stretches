1d7625b10c8b455a253503cebb793b8a9be1ba67
chadalad <cnuckols7@gmail.com>
2020-05-02 09:18:00 -0400

    committing palindrome

--- questions/04_palindrome/palindrome.js
+++ questions/04_palindrome/palindrome.js
@@ -3,6 +3,20 @@
 
 const validPalindrome = () => {
   //YOUR CODE
+  
+  let str = Array.from(arguments).toString;
+
+  let pointer1 = '';
+  let pointer2 = '';
+
+  for (let i = 0; i < str.length; i ++) {
+    pointer1 = str[i];
+    pointer2 = str[str.length - 1 - i];
+    if (pointer1 !== pointer2) {
+      return false;
+    }
+  }
+  return true;
 };
 
 module.exports = { validPalindrome };
