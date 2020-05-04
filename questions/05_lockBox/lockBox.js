15a8f0ebd433f71ab2a2cc1f4cb18cee637ce676
chadalad <cnuckols7@gmail.com>
2020-05-04 18:57:34 -0400

    lockbox initial

--- questions/05_lockBox/lockBox.js
+++ questions/05_lockBox/lockBox.js
@@ -1,5 +1,26 @@
-const lockBox = () => {
+const lockBox = (code, description) => {
   // write code here
+  if (typeof(code) !== Number) {
+    throw 'invalid arg types';
+  }
+  else if (typeof(description) !== String) {
+    throw 'invalid arg types';
+  }
+
+  const modCode = (code, newCode) => {
+    code = newCode;
+    return code;
+  }
+
+  const modMessage = () => {
+
+  }
+
+  const revealMessage = () => {
+
+  }
+
+
 };
 
 module.exports = { lockBox };
