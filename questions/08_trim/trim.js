cde7774e158f7f288f05d2b553e7d585860d09bb
chadalad <cnuckols7@gmail.com>
2020-05-08 00:22:28 -0400

    commit of trim

--- questions/08_trim/trim.js
+++ questions/08_trim/trim.js
@@ -2,6 +2,15 @@
 
 const trim = obj => {
   // YOUR CODE
+  let retObj = {};
+
+  for(let key in obj) {
+    if (obj[key] !== undefined && obj[key] !== null) {
+      retObj[key] = obj[key];
+    }
+  }
+
+  return retObj;
 };
 
 module.exports = { trim };
