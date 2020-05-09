735b7904085494b4aca006f2e33fac7da0a3a7e3
chadalad <cnuckols7@gmail.com>
2020-05-09 09:11:58 -0400

    splatter

--- questions/10_splatter/splatter.js
+++ questions/10_splatter/splatter.js
@@ -2,6 +2,16 @@
 
 const splatter = () => {
   //write code here
+  const args = [...arguments];
+  console.log(args);
+  args.reduce((acc, current) => {
+    Object.values(current).forEach(elem => {
+      acc = acc + elem;
+    });
+    console.log(acc);
+    return acc;
+  }, 0)
+
 };
 
 module.exports = { splatter };
