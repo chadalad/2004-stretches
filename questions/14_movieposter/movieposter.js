72699c3ea72604c05b44e140c678939f4f31b3b1
chadalad <cnuckols7@gmail.com>
2020-05-13 18:51:30 -0400

    movieposter

--- questions/14_movieposter/movieposter.js
+++ questions/14_movieposter/movieposter.js
@@ -7,8 +7,33 @@
 // * Play *
 // ********
 
-const movieposter = () => {
+const movieposter = (arr) => {
   //write code here
+  if(arr === undefined || arr.length == 0) throw new Error('invalid arr');
+  let retStr = '';
+
+  let maxLength = 0;
+
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i].length === 0) {
+      maxLength = arr[i].length;
+    }
+  }
+
+  for(let i = 0; i < maxLength + 2; i++) {
+    retStr += '*';
+  }
+
+  for(let i = 0; i < arr.length; i++) {
+    retStr += `* ${arr[i]}`;
+    retStr += '/n';
+  }
+
+  for(let i = 0; i < maxLength + 2; i++) {
+    retStr += '*';
+  }
+
+  return retStr;
 };
 
 module.exports = { movieposter };
