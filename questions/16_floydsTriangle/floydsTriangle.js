0401f190061faa2482fcf6a6383dc56c543248f3
chadalad <cnuckols7@gmail.com>
2020-05-15 16:47:01 -0400

    floyds

--- questions/16_floydsTriangle/floydsTriangle.js
+++ questions/16_floydsTriangle/floydsTriangle.js
@@ -6,6 +6,21 @@
 //  23
 //  456
 
-const floydsTriangle = n => {};
+const floydsTriangle = n => {
+    let retStr = '';
+    let count = 1;
+    for (let i = 0; i < n; i++) {
+        let rowStr = '';
+        for(let j = 0; j < i + 1; j++) {   
+            rowStr = rowStr + count;
+            count++;
+        }
+        //console.log(rowStr, n, count);
+        if (i !== n - 1) rowStr += '\n';
+        retStr += rowStr;
+    }
+    console.log(`n = ${n}, count = ${count}, retStr = ${retStr}`);
+    return retStr;
+};
 
 module.exports = { floydsTriangle };
