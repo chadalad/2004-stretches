670ddab63abc59b8d1441d6e757d82c51a5e7ccf
chadalad <cnuckols7@gmail.com>
2020-05-16 09:11:38 -0400

    bookReview

--- questions/17_bookReview/bookReview.js
+++ questions/17_bookReview/bookReview.js
@@ -1,2 +1,21 @@
-class BookReview {}
+class BookReview {
+    constructor(title, author) {
+        this.title = title;
+        this.author = author;
+        this.notes = [];
+        this.rating;
+    }
+
+    addNotes(num, note) {
+        if (this.notes[num] === undefined) { this.notes[num] = note; }
+        else { this.notes[num] += ` ${note}`; }
+    }
+
+    addRating(rating) {
+        if (rating <= 5 && rating > 0) {
+            this.rating = rating;
+        }
+        else { throw new Error('invalid rating'); }
+    }
+}
 module.exports = { BookReview };
