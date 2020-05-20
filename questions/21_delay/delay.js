cff68b8cabc9c3d721ed56d85c4fdbec0abbbc7e
chadalad <cnuckols7@gmail.com>
2020-05-20 18:50:33 -0400

    delay

--- questions/21_delay/delay.js
+++ questions/21_delay/delay.js
@@ -1,5 +1,14 @@
 const delay = (str, time) => {
   //Your code here
+  
+
+    return new Promise((resolve) => {
+      setTimeout(
+        resolve,
+        time)
+    }).then(() => {
+      console.log(str);
+    })
 };
 
 module.exports = { delay };
