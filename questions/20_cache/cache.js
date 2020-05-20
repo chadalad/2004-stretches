4959a85ecc5164fd975200256b33b85b2c24c76c
chadalad <cnuckols7@gmail.com>
2020-05-20 00:58:20 -0400

    cache

--- questions/20_cache/cache.js
+++ questions/20_cache/cache.js
@@ -1,7 +1,25 @@
 // see test specs
 
-function cache() {
+let cashh = {};
+
+function cache(func) {
   // YOUR CODE
+  if (typeof func !== 'function') throw new Error('Input must be a function.');
+  //let args = [...arguments];
+  //let cache = [];
+
+  return (val) => {
+    //console.log(Object.keys(cache))
+    //console.log(Object.entries(cashh))
+    console.log(Object.keys(cashh), val)
+    if (!Object.keys(cashh).includes(val.toString())) {
+      cashh[val] = func(val);
+      return cashh[val];
+    } else {
+      return cashh[val];
+    }
+    //return func(val);
+  };
 }
 
 module.exports = { cache };
