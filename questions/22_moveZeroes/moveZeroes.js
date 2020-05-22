2e0c764e85a120005164434d27c8d63d807ae931
chadalad <cnuckols7@gmail.com>
2020-05-22 01:01:00 -0400

    moveZeroes

--- questions/22_moveZeroes/moveZeroes.js
+++ questions/22_moveZeroes/moveZeroes.js
@@ -1,5 +1,25 @@
 const moveZeroes = (arr) => {
   //YOUR CODE HERE
+//sort 
+const zeroArr = [];
+const numArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 0) {
+      zeroArr.push(arr[i]);
+    } else {
+      numArr.push(arr[i]);
+    }
+  }
+  for (let i = 0; i < numArr.length; i++) {
+    arr[i] = numArr[i];
+  }
+  for (let i = 0; i < zeroArr.length; i++) {
+    arr[numArr.length + i] = zeroArr[i];
+  }
+  console.log(zeroArr, numArr, arr);
+
+  return arr;
 };
 
 module.exports = { moveZeroes };
