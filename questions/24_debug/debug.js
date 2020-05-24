d9faf65fe3258c2d02c6de78a0773f07a9e48648
chadalad <cnuckols7@gmail.com>
2020-05-24 02:24:51 -0400

    debug

--- questions/24_debug/debug.js
+++ questions/24_debug/debug.js
@@ -1,24 +1,25 @@
 //YOU WILL BE EDITING THIS CODE
 
 class CoffeeShop {
-  constructer() {
-    this.pendingOrders = '';
+  constructor() {
+    this.pendingOrders = [];
     this.completedOrders = [];
     this.menu = ['mocha', 'latte', 'flat-white', 'drip', 'espresso'];
   }
 
   inputOrder(str) {
-    this.pendingOrders.push();
+    this.pendingOrders.push(str);
   }
 
   makeCoffee() {
-    const cofeeOrder = pendingOrders.shift();
-
-    this.completedOrders.push(coffeeorder);
+    if (this.pendingOrders.length > 0) {
+      const coffeeOrder = this.pendingOrders.shift();
+      this.completedOrders.push(coffeeOrder);
+    }
   }
 
   printMenu() {
-    return menu;
+    return this.menu;
   }
 }
 
