b116d27576ea55e8b52dbbbb031cf552c33b11aa
chadalad <cnuckols7@gmail.com>
2020-05-26 00:47:45 -0400

    setState

--- questions/25_setState/setState.js
+++ questions/25_setState/setState.js
@@ -6,8 +6,34 @@ class StatefulThing {
   constructor(initialState = {}) {
     this.state = initialState;
   }
-  setState() {
+  setState(stateObj) {
     // YOUR CODE
+    // const newState;
+    // this.state = newState;
+    //console.log(this.state)
+    //console.log(Object.keys(stateObj))
+    //this.followingState = Object.from(this.state);
+    //console.log(this.followingState, "follow")
+    this.nextState = {};
+    Object.entries(this.state).forEach(element => {
+      if (stateObj[element[0]]) { 
+        this.nextState[element[0]] = stateObj[element[0]]; 
+      }
+      else {
+        this.nextState[element[0]] = element[1]
+      }
+      
+    })
+    console.log(this.nextState)
+    Object.keys(stateObj).forEach(element => {
+      //console.log(element);
+      //console.log(this.state[element])
+      this.state[element] = stateObj[element];
+      //console.log(this.state[element])
+    });
+    console.log(this.state);
+   // console.log(this.followingState);
+    return this.nextState;
   }
 }
 
