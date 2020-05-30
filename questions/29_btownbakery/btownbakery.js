611cae9e8786727fa3f46a54a924d67974fbb680
chadalad <cnuckols7@gmail.com>
2020-05-30 09:20:42 -0400

    bakery

--- questions/29_btownbakery/btownbakery.js
+++ questions/29_btownbakery/btownbakery.js
@@ -1,7 +1,23 @@
 // B-Town bakery over-bought the supplies to make cake and needs to know how many they can make all together.
 
-const btownbake = () => {
+const btownbake = (recipe, supplies) => {
   //enter code here
+  if(typeof recipe !== 'object' || typeof supplies !== 'object') {
+    throw new Error('arg not obj');
+  }
+  // compare recipes and supplies, which is base?
+  let creationCount = [];
+  //iterate over the object - only really care about what we have
+
+  recipe.forEach(elem => {
+    let count = 0;
+    if (supplies[elem]) {
+      count = Math.floor(supplies[elem] / recipe[elem]);
+    }
+    creationCount.push({ elem : count });
+  })
+  console.log(creationCount);
+
 };
 
 module.exports = { btownbake };
