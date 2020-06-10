3bb21213b94989b08ab37fa59ff596c27c458d6a
chadalad <cnuckols7@gmail.com>
2020-06-10 18:30:04 -0400

    commit

--- questions/37_cipher2/cipher2.js
+++ questions/37_cipher2/cipher2.js
@@ -1,6 +1,12 @@
 const decypher = (str => {
   //YOUR CODE HERE
   const retStr = '';
+  const pointer = 0;
+  for(let i = 0; i < str.length;)
+  const numSeq = Array.from(str).reduce(acc, (elem) => {
+    if (typeof elem === 'number')
+  }, '')
+  String.fromCharCode()
   return retStr;
 });
 module.exports = { decypher };
