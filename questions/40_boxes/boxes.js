6bd24d301c734105034ba3cf96e5d3407d259616
chadalad <cnuckols7@gmail.com>
2020-06-13 09:15:57 -0400

    boxes

--- questions/40_boxes/boxes.js
+++ questions/40_boxes/boxes.js
@@ -1,3 +1,20 @@
 //YOUR CODE HERE
+class Box {
+    constructor(color, capacity) {
+        this.color = color;
+        this.capacity = capacity;
+        this.contents = [];
+    }
+
+    pack(color, capacity) {
+        if (this.contents.length === capacity) {
+            throw new Error('capacity met');
+        } else {    
+            const packedBox = new Box(color, capacity);
+            this.contents.push(packedBox);
+        }
+        
+    }
+}
 
 module.exports = { Box };
