d70bfbbbee15caab46c53fd84465a42ba655a651
chadalad <cnuckols7@gmail.com>
2020-06-24 18:54:44 -0400

    depthmap

--- questions/50_depthmap/depthmap.js
+++ questions/50_depthmap/depthmap.js
@@ -2,8 +2,20 @@
 //Write a function to recursively "dig" through this object installing a depth key along the way
 //return the object with it's depth indicated
 
-const recordDepth = () => {
+const recordDepth = (obj) => {
   //write code here
+  const depthVal;
+  if (typeof obj !== 'object') throw new Error('object required as entry');
+  //go through and see how many
+  //what is base case? it is to see what the deepest portion is. There are no more objects
+  console.log(Object.entries(obj).length);
+  // for (let i = 0; i < Object.entries(obj).length; i++) {
+  //   console.log(Object.values(obj).includes(typeof 'object'))
+  // }
+  for(let i = 0; i < Object.values(obj).length; i++) {
+
+  }
+
 };
 
 module.exports = { recordDepth };
